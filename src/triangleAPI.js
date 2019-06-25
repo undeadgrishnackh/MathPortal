d3e6f195bec1a293c83809eb4921b40e577d06a7
Michele Brissoni <undeadgrishnackh@gmail.com>
2019-06-25 17:47:06 +0200

    Unit Test skeleton done - RED

--- src/triangleAPI.js
+++ src/triangleAPI.js
@@ -1,18 +1,18 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 var PORT = 8888;
 
 // /api/MathPortal/triangle/3thAngle/1.0.0
 // /api/v1/todos
-app.post("/api/MathPortal/triangle/3thAngle/1.0.0", (req, res) => {
-    res.status(200).send({
-        delta: "FAKE_MOCK_TO_CHECK_THE_NEWMAN_TEST"
-    });
+app.post('/api/MathPortal/triangle/3thAngle/1.0.0', (req, res) => {
+  res.status(200).send({
+    delta: 'FAKE_MOCK_TO_CHECK_THE_NEWMAN_TEST'
+  });
 });
 
 app.listen(PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log(`server running on port ${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`server running on port ${PORT}`);
 });
 
 module.exports = app;
---
+++ test/triangle.test.js
@@ -0,0 +1,10 @@
+// const { triangleCalculator } = require('../src/triangleCalculator');
+const { assert } = require('chai');
+
+describe('Calculate angles for a well formed triangle.', () => {
+  it('90 degrees triangle.', () => {
+    assert.fail('TEST NOT YET IMPLEMENTED!');
+  });
+});
+
+
