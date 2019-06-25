324f73182d38a0a0e2e17c009c763d423767f593
Michele Brissoni <undeadgrishnackh@gmail.com>
2019-06-25 17:22:53 +0200

    before starting UNIT TEST

---
+++ features/triangle_steps.js
@@ -0,0 +1,47 @@
+// features/support/steps.js
+const { Given, When, Then } = require("cucumber");
+// const { expect } = require("chai");
+
+Given("a triangle without any angle", function () {
+    return "pending";
+});
+
+When("the student insert the first angle {int}", function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+});
+
+When("after it the second angle {int}", function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+});
+
+When("ask for calculate", function (){
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+});
+
+Then("the last angle should contain {int}", function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+});
+
+When("the student insert an angle {int}", function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+});
+
+When("the sum of them is equal or more than {int} degrees", function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+});
+
+Then("the last angle should contain ERROR", function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+});
+
+When("one of them is equal or less than {int} degrees", function () {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+});
--- src/triangleAPI.js
+++ src/triangleAPI.js
@@ -1,5 +1,18 @@
-class TriangleAPI {
+var express = require("express");
+var app = express();
+var PORT = 8888;
 
-}
+// /api/MathPortal/triangle/3thAngle/1.0.0
+// /api/v1/todos
+app.post("/api/MathPortal/triangle/3thAngle/1.0.0", (req, res) => {
+    res.status(200).send({
+        delta: "FAKE_MOCK_TO_CHECK_THE_NEWMAN_TEST"
+    });
+});
 
-module.exports = TriangleAPI;
+app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`server running on port ${PORT}`);
+});
+
+module.exports = app;
